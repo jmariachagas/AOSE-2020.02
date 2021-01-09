2bebbbb0d4857c8f800636f5e0e37d7bec4260b9
DESKTOP-14L06C0\User <jonaschagas10@gmail.com>
2021-01-09 03:05:18 -0300

    Adicionando elementos de inicializacao do ambiente

--- Desenvolvimento/projeto_jogos/src/java/Ambiente.java
+++ Desenvolvimento/projeto_jogos/src/java/Ambiente.java
@@ -1,5 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import jason.asSyntax.Literal;
 import jason.asSyntax.Structure;
@@ -13,9 +16,17 @@ public class Ambiente extends Environment{
 	
 	private ModeloCasaInfestada modeloCasaInfestada;
 	private VisaoAmbiente visaoAmbiente;
-	Location donaCasa;
-	public static final Term  pc = Literal.parseLiteral("proximaCasa");
+	private static final int QUEIJO = 8;
+	private static final int EntradaSaida = 16;
+	private static final int BURACO = 32;
 	
+	List<Integer> listX = new ArrayList<Integer>();
+	 List<Integer> listY = new ArrayList<Integer>();
+	Location donaCasa;	
+	
+	public static final Term  pc = Literal.parseLiteral("proximaCasa");	
+	private List<Integer> resultPosicaoQueijos = new ArrayList<Integer>();
+	private List<Integer> resultPosicaoBuracos = new ArrayList<Integer>();
 	
 	/**
 	 * metodo relacionado a classe Environment
@@ -27,10 +38,21 @@ public class Ambiente extends Environment{
 		modeloCasaInfestada = new ModeloCasaInfestada(20, 20, 1); //modificar o numero de agentes conforme for incrementando
 		visaoAmbiente = new VisaoAmbiente(modeloCasaInfestada);
 		modeloCasaInfestada.setView(visaoAmbiente);
-		
+				
 		Location donaCasaLoc = modeloCasaInfestada.getAgPos(0);
         Literal pos1 = Literal.parseLiteral("pos(donaCasa," + donaCasaLoc.x + "," + donaCasaLoc.y + ")");
         addPercept(pos1);
+        modeloCasaInfestada.add(EntradaSaida, 0, 0);
+        for(int i = 0; i < 12; i++) {
+        	resultPosicaoQueijos = posicionaElementos();        	
+        	modeloCasaInfestada.add(QUEIJO, resultPosicaoQueijos.get(0), resultPosicaoQueijos.get(1));        	
+        }
+        
+        for (int i = 0; i < 4; i++) {
+        	resultPosicaoBuracos = posicionaElementos();
+        	modeloCasaInfestada.add(BURACO, resultPosicaoBuracos.get(0), resultPosicaoBuracos.get(1));
+        }
+        
 	}
 	
 	/**
@@ -60,6 +82,39 @@ public class Ambiente extends Environment{
 		 
 		 super.stop();		 
 	 }
+	 
+	 public List<Integer> posicionaElementos() {		 
+		 
+		 List<Integer> result = new ArrayList<Integer>();
+		 int x = 0;
+		 int y = 0;
+		 
+		 Random r = new Random();
+		 x = r.nextInt(19);
+		 y = r.nextInt(19);
+		 
+		 for(Integer n : listX) {
+			 if(n == x) {
+				 while(n == x) {
+					 x = r.nextInt(19);
+				 }
+			 }
+		 }
+		 
+		 for (Integer n : listY) {
+			if(n == y) {
+				while(n == y) {				
+					y = r.nextInt(19);
+				}
+			}
+		}
+		 listX.add(x);
+		 listY.add(y);
+		 result.add(x);
+		 result.add(y);
+		 
+		 return result;		 
+	 }
 	
 	/**
 	 * Classe que implementa o GridWorldModel onde define o modelo do ambiente
@@ -75,6 +130,7 @@ public class Ambiente extends Environment{
 			//inicializar aqui a posicao inicial dos agentes
 			
 			try {
+				
 				setAgPos(0, 0, 0); // Posiciona o primeiro agente na posição 0,0
 			} catch(Exception e) {
 				 e.printStackTrace();
@@ -132,7 +188,39 @@ public class Ambiente extends Environment{
 		@Override
 		public void draw(Graphics g, int x, int y, int object) {
 			
+			switch(object) {
+				case Ambiente.QUEIJO:
+					criarQueijo(g, x, y);
+					break;
+				case Ambiente.EntradaSaida:
+					criarEntradaSaida(g, x, y);
+					break;
+				case Ambiente.BURACO:
+					criarBuracosRatos(g, x, y);
+					break;
+			}			
 		}
+		
+		public void criarQueijo(Graphics g, int x, int y) {
+			
+			super.drawObstacle(g, x, y);
+			g.setColor(Color.yellow);		        
+			drawString(g, x, y, defaultFont, "Queijo");
+			
+		}
+		
+		public void criarEntradaSaida(Graphics g, int x, int y) {
+			super.drawObstacle(g, x, y);
+			g.setColor(Color.ORANGE);		        
+			drawString(g, x, y, defaultFont, "Entrada/Saida");
+		}
+		
+		public void criarBuracosRatos(Graphics g, int x, int y) {
+			super.drawObstacle(g, x, y);
+			g.setColor(Color.white);		        
+			drawString(g, x, y, defaultFont, "Buraco");
+		}
+		
 		
 		/**
 		 * metodo relacionado a classe GridWorldView
